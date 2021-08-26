64f0aa7f0162126405673672d8b86697041a0c65
bcyart <71490410+bcyart@users.noreply.github.com>
2021-08-26 16:50:28 +0300

    Update list.js

--- src/components/list.js
+++ src/components/list.js
@@ -159,7 +159,7 @@ export const droneList = [
       id: 1,
       title: "Türkiye İş Kurumu - Açık İş İlanı",
       path: "/Türkiye İş Kurumu - Açık İş İlanı",
-      video: "https://player.vimeo.com/video/422947968?title=0&byline=0&portrait=0"
+      video: "<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/422947968?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479&amp;h=7616958541" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="İşkur - Video Ads"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>h"
      
     },
     {

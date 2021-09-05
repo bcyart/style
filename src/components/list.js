2db85fc68595fdbf78b999daa397d50abf812a6e
bcyart <71490410+bcyart@users.noreply.github.com>
2021-09-05 22:12:40 +0300

    Update list.js

--- src/components/list.js
+++ src/components/list.js
@@ -85,7 +85,7 @@ export const droneList = [
       id: 0,
       title: "UNFPA - kırsala Destek ",
       path: "/UNFPA - kırsala Destek ",
-      video:"https://player.vimeo.com/video/496421591?title=0&byline=0&portrait=0"
+      video:"https://player.vimeo.com/video/589299298?title=0&byline=0&portrait=0"
     },
     
     {

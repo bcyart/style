51ec045eeb22d0dbc2ea06f9288f685dd9318f4d
bcyart <71490410+bcyart@users.noreply.github.com>
2024-08-25 22:40:58 +0300

    Update list.js

--- src/components/list.js
+++ src/components/list.js
@@ -1,45 +1,5 @@
 import React from 'react'
 
-export const bannerList = [
-  {
-    id: 0,
-    title: "banner1",
-    path: "/banner2",
-    image:"https://live.staticflickr.com/65535/50343188162_002d088e9b_z.jpg"
-  },
-  {
-    id: 1,
-    title: "banner2",
-    path: "/banner2",
-    image: "https://live.staticflickr.com/65535/50343188417_00c2cf8788_z.jpg"
-   
-  },
-  {
-    id: 2,
-    title: "banner3",
-    path: "/banner3",
-    image:"https://live.staticflickr.com/65535/50343028526_87549dbcf3_z.jpg"
- 
-  },
-  {
-    id: 3,
-    title: "banner4",
-    path: "/banner4",
-    image: "https://live.staticflickr.com/65535/50343030806_b312fd25a1_z.jpg"
-
-  },
-
-  
-  {
-    id: 4,
-    title: "banner5",
-    path: "/banner5",
-    image:"https://live.staticflickr.com/65535/50343188247_3f6a42cb73_z.jpg"
-
-  }
-];
-
-
 export const droneList = [
     {
       id: 0,
